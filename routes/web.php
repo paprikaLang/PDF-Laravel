69b38a2f846e49321832e7d83edcd1374013dcf9
paprikaLang <3060866223@qq.com>
2017-11-07 03:40:00 +0800

    laravel生成PDF

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+use Illuminate\Support\Facades\App;
+use Barryvdh\Snappy\Facades\SnappyImage;
+use Barryvdh\Snappy\Facades\SnappyPdf;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $snappy = App::make('snappy.pdf');
+    //to file
+    $html = '<h1>Paprika</h1><p>Hello World!</p>';
+    $snappy->generateFromHtml($html,'/tmp/hello.pdf');
+    $snappy->generate('http://www.github.com','/tmp/github.pdf');
+    //output
+    return new \Illuminate\Http\Response(
+        $snappy->getOutputFromHtml($html),
+        200,
+        array(
+            'Content-Type'   =>'application/pdf',
+            'Content-Disposition'  => 'attachment; filename="file.pdf"'
+        )
+    );
+});
